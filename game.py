8e5c2c7fc48a80226fceac836257670fc050f6c5
keeran-richardson <66531762+keeran-richardson@users.noreply.github.com>
2021-03-14 13:24:50 +0000

    More tkinter
    fully implemented tkinter and turned procedural logic to event based.

--- game.py
+++ game.py
@@ -1,13 +1,19 @@
 import random
 import tkinter as tk
 from tkinter import simpledialog
-from tkinter import scrolledtext as st 
+from tkinter import scrolledtext as st
 
-characters=["Professor Plum", "Mrs.White", "Mr Green", "Mrs.Peacock", "Miss Scarlett", "Colonel Mustard"]
+characters=["Professor Plum", "Mrs White", "Mr Green", "Mrs Peacock", "Miss Scarlett", "Colonel Mustard"]
 weapons=["Wrench", "Candlestick", "Lead Pipe", "Rope", "Revolver", "Knife"]
 rooms=["Study", "Kitchen", "Hall", "Conservatory", "Lounge", "Ballroom", "Dining Room", "Library", "Billiard Room"]
 
+application_window = tk.Tk()
+application_window.title("cluedo")
+application_window.config(bg = "Purple")
+application_window.geometry("750x400")
 
+gameState = "notStarted"
+guesses = "N/A"
 
 def generateMurder(characters, weapons, rooms):
     murderer=random.choice(characters)
@@ -16,85 +22,148 @@ def generateMurder(characters, weapons, rooms):
     crime=[murderer, weapon, room]
     return crime
 
-def inputGuess(guesses, options, question):
-    tkinterPrint("you have "+str(guesses)+" guesses left")
+def tkinterPrint(message):
+    global application_window
+    print(message)
+    text_area.insert(tk.END, message)
+    text_area.insert(tk.END, "\n")
+    text_area.see(tk.END)
+    
+
+def getValidAnswer(options):
+    guess=entry.get()
+    entry.delete(0, 'end')
+    if guess not in options:
+        tkinterPrint(guess+ " is not a valid input")
+        return ""
+        
+    return guess
+
+def inputGuess(options, question):
     prompt = question + "\n"
     questionNumber = 1
     for option in options:
         prompt += str(questionNumber) + ". " + option + "\n" 
         questionNumber += 1
     
-    return getValidAnswer(prompt, options)
-
-def getValidAnswer(prompt, options):
-
-    guess=""
-    while guess not in options:
-        if guess != "":
-            tkinterPrint("That is not a valid input")
-        guess=tkinterInput(prompt)
-            
-    return guess
-
-def tkinterInput(prompt, title="Input"):
-    global application_window
-
-    answer = simpledialog.askstring(title, prompt)
-
-    return answer
-
-def tkinterPrint(message):
-    global application_window
-    print(message)
-    text_area.insert(tk.INSERT, message)
-    text_area.insert(tk.INSERT, "\n")
-    text_area.see(tk.END)
+    tkinterPrint(prompt)
+    
 
 def checkGuess(guess, answer):
-    if guess==answer:
-        tkinterPrint("you got it right")
+    if guess.upper()==answer.upper():
+        tkinterPrint("You got it right!")
         return True
     else:
-        tkinterPrint("you got it wrong")
+        tkinterPrint("You got it wrong!")
         return False
 
-def guessCrime(crimeScene, characters, weapons, rooms, guesses):
-    correctGuesses=0
-
-    questions=("Who do you think the murderer was?", "What do you think the weapon was?", "Which room do you think it was?")
-    crimeList=(characters, weapons, rooms)
-    questionNumber=0
-
-    while guesses>0:
-        guessAnswer=inputGuess(guesses, crimeList[questionNumber], questions[questionNumber])
+def guessCrime(questions, crimeList, crimeScene, characters, weapons, rooms):
+    global correctGuesses
+    global questionNumber
+    global gameState
+    global guesses
+    if guesses>0:
+        guessAnswer=getValidAnswer(crimeList[questionNumber])
+        if guessAnswer == "":
+            return
         guesses-=1
         if checkGuess(guessAnswer, crimeScene[questionNumber]):
             correctGuesses+=1
             questionNumber+=1
-        if correctGuesses == 3:
-            break
-        
-    tkinterPrint("you got "+str(correctGuesses)+" guesses right")
 
 def chooseLevel():
 
-    difficulty={"easy":15,"medium":10,"hard":5}
- 
-    return difficulty[getValidAnswer("Do you want easy, medium or hard difficulty?\n", difficulty)]
+    difficulty={"easy":13,"medium":8,"hard":3}
+    
+    validAnswer = getValidAnswer(difficulty)
+    if validAnswer != "":
+        return difficulty[validAnswer]
+    return ""
 
-application_window = tk.Tk()
-application_window.title("cluedo")
-text_area = st.ScrolledText(application_window, width = 30, height = 8) 
-  
-text_area.grid(column = 0, pady = 10, padx = 10) 
+def playgame(event = None):
+    global gameState
+    global guesses
+    global characters
+    global weapons
+    global rooms
+    global crimeScene
+    global correctGuesses
+    global questionNumber
+    global questions
+    global crimeList
 
-while True:   
-    crimeScene=generateMurder(characters, weapons, rooms)
-    print(crimeScene)
-    guesses = chooseLevel()
-    guessCrime(crimeScene, characters, weapons, rooms, guesses)
-    playAgain=tkinterInput("do you want to play again? y/n")
-    if playAgain == "y":
-        continue
-    else:
-        break
+    if gameState=="notStarted":
+        tkinterPrint("Click on the Start Game button!")
+        return
+
+    if gameState=="started":
+        button2.config(text="Restart Game")
+        tkinterPrint("Hello and welcome to Keeran's Cluedo!\nFirst you need to select a difficulty, do you want easy, medium or hard?")
+        gameState = "chooseDificulty"
+        crimeScene = generateMurder(characters, weapons, rooms)
+        print(crimeScene)
+        correctGuesses=0
+        questionNumber=0
+        return
+
+    if gameState=="chooseDificulty":
+        guesses = chooseLevel()  
+        if guesses != "":
+            label2.config(text = ("Guesses remaining:", guesses))
+            gameState = "askQuestion1"
+            questions=("Who do you think the murderer was?", "What do you think the weapon was?", "Which room do you think it was?")
+            crimeList=(characters, weapons, rooms)
+            inputGuess(crimeList[questionNumber], questions[questionNumber])
+        return
+
+    if gameState=="askQuestion1":
+        guessCrime(questions, crimeList, crimeScene, characters, weapons, rooms)
+        label2.config(text = "Guesses remaining: "+ str(guesses))
+        if guesses == 0 and correctGuesses != 3:
+            tkinterPrint("You lost the game!")
+            gameState = "startGame"
+            tkinterPrint("Click 'Restart Game' to restart!")
+            return
+        if questionNumber < 3:
+            inputGuess(crimeList[questionNumber], questions[questionNumber])
+        else:
+            if correctGuesses == 3:
+                tkinterPrint("You won the game!")
+                gameState = "startGame"
+                tkinterPrint("Click 'Restart Game' to restart!")
+            else:
+                tkinterPrint("You lost the game!")
+                gameState = "startGame"
+                tkinterPrint("Click 'Restart Game' to restart!")
+        return
+                       
+def restart():
+    global gameState
+    global guesses
+    guesses = "N/A"
+    gameState = "started"
+    playgame()
+
+label1 = tk.Label(application_window, text="Keeran's Cluedo!", font =("Arial", 44), bg = "Purple", fg = "White")
+label1.pack()
+
+button2 = tk.Button(text="Start Game", command=restart, bg = "Yellow", fg = "Black")
+button2.pack()
+
+entry = tk.Entry(application_window)
+entry.pack()
+
+application_window.bind('<Return>', playgame)
+
+button1 = tk.Button(text="Enter", command=playgame, bg = "Yellow", fg = "Black")
+button1.pack()
+
+label2 = tk.Label(application_window, text=guesses, bg = "Purple", fg = "White")
+label2.pack()
+
+text_area = st.ScrolledText(application_window) 
+text_area.pack() 
+
+application_window.mainloop()
+
+    
